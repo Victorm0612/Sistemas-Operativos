451037f378fc840af035fce564bd6b11de0decad
Victorm0612 <rojasvictorvargas@gmail.com>
2019-06-17 23:07:27 -0500

    Adicionando archivos cabecera threads

--- tarea_01_ostep/common_threads.h
+++ tarea_01_ostep/common_threads.h
@@ -9,6 +9,7 @@
 #include <semaphore.h>
 #endif
 
+/*DEFINICIONES PARA LA CREACIÓN DE  HILOS*/
 #define Pthread_create(thread, attr, start_routine, arg) assert(pthread_create(thread, attr, start_routine, arg) == 0);
 #define Pthread_join(thread, value_ptr)                  assert(pthread_join(thread, value_ptr) == 0);
 
