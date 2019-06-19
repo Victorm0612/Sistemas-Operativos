5cc8559762993870f37d046ff1160605d727400b
camilogonzalez96 <juan.berrio@correounivalle.edu.co>
2019-06-19 11:37:23 -0500

    Correción lógica de bucle

--- tarea_01_ostep/io.c
+++ tarea_01_ostep/io.c
@@ -6,21 +6,22 @@ int main(int argc, char *argv[])
  {
   FILE *archivo;
   int Lp, posActual; //Lp = LastPosition ; posActual = PosiciónActual
-  posActual=0;
+  posActual=1;
 
 //VALIDACIÓN-EXISTENCIA DE ARCHIVO DE TEXTO//
 if((archivo = fopen(argv[1],"r"))!=NULL)
      {
          archivo = fopen(argv[1],"r");
-         fseek(archivo,0,SEEK_END);
+         fseek(archivo,-2,SEEK_END);
          Lp = ftell(archivo);
-         while(posActual<Lp)
+         while(posActual<=Lp)
           {
            printf("%c",fgetc(archivo));
            fseek(archivo,-(posActual+2),SEEK_END);
            posActual++;
-
           }
+         fseek(archivo,-posActual,SEEK_CUR);
+         printf("%c", fgetc(archivo));
          printf("\n");
          fclose(archivo);
          return 0;
