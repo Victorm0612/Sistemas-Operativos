97b3cbbbd611f739e46f7d22fcdef8bcb2cdb613
Victorm0612 <rojasvictorvargas@gmail.com>
2019-06-24 13:06:01 -0500

    Adicionando validación.

--- Ejercicios-Programacion-C05/fork_03.c
+++ Ejercicios-Programacion-C05/fork_03.c
@@ -7,7 +7,11 @@ int main(int argc, char** argv)
  {
   pid_t pid;
   pid = fork();
-  if(pid == 0)
+  if(pid < 0)
+   {
+    fprintf(stderr, "fork fallido.\n");
+   }
+  else if(pid == 0)
    {
     printf("hello.\n");
    }
