4d0b76c638699ee7d7edda96bfe1de6362b0a910
Victorm0612 <rojasvictorvargas@gmail.com>
2019-06-30 12:56:11 -0500

    Adicionando casos base

--- Proyecto-Shell/shell.c
+++ Proyecto-Shell/shell.c
@@ -13,22 +13,27 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdlib.h>
+#include <readline/readline.h> 
+#include <readline/history.h> 
+#include <string.h>
 
-void gramatica(char* comando)
-{
- //Numero de cadenas x Tamaño max. de cadena
- char comandos[20][10] = {"ls", "cd","clr","dir","environ","echo", "help","pause","quit"};
+int Numero_caracteres = 500;
+int Numero_comandos   = 100;
 
-}
+int entrada(char* command)
+{ 
+   command = readline("\nprompt> "); 
+//strlen toma el argumento y devuelve la longitud de este (hay que ver si me pasan algo o no) 
+    if (strlen(command) != 0) {
+      //Con add_history puedo acceder a un comando ya utilizado  
+        add_history(command); 
+        return 0; 
+    } else { 
+        return 1; 
+    } 
+} 
 
-int main(char* command)
-{
-  printf("prompt> ");
-  scanf("%s",command);  
-  return 0;
-}
-
-void ejecucion_comando(char** comando)
+void ejecucion_comando(char** comandos)
 {
   pid_t pid = fork();
   if(pid < 0)
@@ -39,11 +44,67 @@ void ejecucion_comando(char** comando)
    //Hijo
   else if(pid == 0)
    {
-    if (execvp(comando[0], comando) < 0)
-    
+    if (execvp(comandos[0], comandos) < 0)
+    {
+     printf("\nNo es posible ejecutar el comando.");
+    }
+    exit(0);
+    }
+  else
+    {
+     wait(NULL);
+    }
    }
 }
 
+int lista_comandos(char** gramatica) 
+{ 
+    int max = 9, posicion = 0; 
+    char* ListaComandos[max]; 
+  
+    ListaComandos[0] = "cd"; 
+    ListaComandos[1] = "clr"; 
+    ListaComandos[2] = "dir"; 
+    ListaComandos[3] = "environ";
+    ListaComandos[4] = "echo";
+    ListaComandos[5] = "help";
+    ListaComandos[6] = "pause";
+    ListaComandos[7] = "quit";
+      
+    for (int i = 0; i < max; i++) { 
+        if (strcmp(gramatica[0], ListaComandos[i]) == 0) { 
+            posicion = i + 1; 
+            break; 
+        } 
+    } 
+  
+    switch (posicion) { 
+    case 1: 
+        return 1; 
+    case 2: 
+        clean(); 
+        return 1; 
+    case 3:
+        execlp(ListaComandos[posicion-1],Listacomandos); 
+        return 1; 
+    case 4: 
+        return 1; 
+    case 5:
+        return 1;
+    case 6:
+        Help();
+        return 1;
+    case 7:
+        return 1;
+    case 8:
+        exit(0);
+    default: 
+        break; 
+    } 
+    return 0; 
+} 
+
+//Mostrando la ayuda
 void Help()
 {
   printf("\n--- BIENVENIDO AL SHELL ---"
@@ -57,5 +118,10 @@ void Help()
          "\n * pause"
          "\n * quit"
          "\n * <ruta>/myshell");
+return 0;
 }
 
+int main()
+{
+  
+}
